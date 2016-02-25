ba006d3cb958f0a4597f669f078a6d2cb90d0c50
Spitfire <jules.boitelle@gmail.com>
2016-02-25 07:43:02 +0100

    <ext>(<main.c>) <update> ' ' Add basic handler of extension file requested. Need now to handler 404 error.' '

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,20 @@
 #include <reckless.h>
 
+t_extensions	extensions[] = 
+{
+	{"gif", "image/gif" },
+	{"jpg", "image/jpeg" },
+	{"jpeg", "image/jpeg" },
+	{"png", "image/png" },
+	{"zip", "image/zip" },
+	{"gz", "image/gz" },
+	{"tar","image/tar" },
+	{"htm", "text/html" },
+	{"html", "text/html" },
+	{"css","text/css"},
+	{0, 0} 
+};
+
 static void		ft_usage(void)
 {
 	dprintf(1, "Usage: ./reckless [PORT]\n");
@@ -49,8 +64,32 @@ static void		web(int fd, int hit)
 		j++;
 	}
 
+	/* Convert no filename "/" to index file. Next check the filetype and review if we support it. */
+	size_t		buffer_length;
+	size_t		len;
+	char		*fstr;
 
-	if ((file_fd = open("index.html", O_RDONLY)) == -1)
+	if (!strncmp(&buffer[0], "GET /\0", 6) || !strncmp(&buffer[0], "get /\0", 6))
+	(void)strcpy(buffer, "GET /index.html");
+
+	buffer_length = strlen(buffer);
+	fstr = NULL;
+	i = 0;
+	while (extensions[i].ext != 0)
+	{
+		len = strlen(extensions[i].ext);
+		if (!strncmp(&buffer[buffer_length - len], extensions[i].ext, len))
+		{
+			fstr = extensions[i].filetype;
+			break;
+		}
+		i++;
+	}
+	if (!fstr)
+	{
+		perror("File extension type not supported yet.");
+	}
+	if ((file_fd = open(&buffer[5], O_RDONLY)) == -1)
 		dprintf(2, "Failed to open file\n");
 	(void)sprintf(buffer, "HTTP/1.0 200 OK\r\nServer:Reckless/0.0.1\r\nContent-Type: %s\r\n\r\n", "text/html");
 	write(fd, buffer, strlen(buffer));
